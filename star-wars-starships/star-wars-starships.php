dde80278e7a79db69b873de35feecb92ed597dd7
omerelias <omerelias123456@gmail.com>
2023-11-02 17:55:51 +0200

    Added shortcode Option

--- star-wars-starships/star-wars-starships.php
+++ star-wars-starships/star-wars-starships.php
@@ -12,11 +12,42 @@ function sws_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'sws_enqueue_scripts');
 
+
+// Fetch Starships data
 function sws_fetch_starships(){
     $url='https://swapi.dev/api/starships/';
     $response = wp_remote_get($url);
     $api_data = wp_remote_retrieve_body($response);
-    var_dump($api_data);
+    
+    if (is_wp_error($api_data)) {
+        return null;
+    }
+
+    $starships = json_decode($api_data, true)['results'];
+    return $starships;
 }
 add_action('init','sws_fetch_starships'); 
+
+function sws_display_starships(){
+    $starships = fetch_starships();
+    if (!$starships) {
+        return 'Failed to fetch starships data.';
+    }
+
+    $data = '<table>';
+    $data.= '<tr>
+    <th>Name</th>
+    <th>Class</th>
+    <th>Crew</th>
+    <th>Cost</th>
+    </tr>';
+
+    foreach ($starships as $starship) {
+        $data.='<tr>';
+        $data.='<td></td>';
+        $data.='</tr>';
+    }
+}
+add_shortcode('star_wars_starships', 'sws_display_starships');
+
 ?>
