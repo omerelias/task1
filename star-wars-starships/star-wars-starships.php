8b9235aada6c41204dc6015593fafdd11ad98cc5
omerelias <omerelias123456@gmail.com>
2023-11-02 17:47:08 +0200

    Fetch data

--- star-wars-starships/star-wars-starships.php
+++ star-wars-starships/star-wars-starships.php
@@ -5,4 +5,18 @@ Description: A plugin to display Star Wars starships from the SWAPI.
 Version: 1.0
 Author: Omer Elias
 */
-?> 
+
+
+function sws_enqueue_scripts() {
+    
+}
+add_action('wp_enqueue_scripts', 'sws_enqueue_scripts');
+
+function sws_fetch_starships(){
+    $url='https://swapi.dev/api/starships/';
+    $response = wp_remote_get($url);
+    $api_data = wp_remote_retrieve_body($response);
+    var_dump($api_data);
+}
+add_action('init','sws_fetch_starships'); 
+?>
