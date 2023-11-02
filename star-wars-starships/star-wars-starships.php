0bdc1b3607abf8999ce5247f5ccbe84f4b9d8805
omerelias <omerelias123456@gmail.com>
2023-11-02 18:25:45 +0200

    Final commit - Designed Table

--- star-wars-starships/star-wars-starships.php
+++ star-wars-starships/star-wars-starships.php
@@ -7,11 +7,11 @@ Author: Omer Elias
 */
 
 
+// Enqueue scripts and styles
 function sws_enqueue_scripts() {
-    
+    wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
 }
 add_action('wp_enqueue_scripts', 'sws_enqueue_scripts');
-
 
 // Fetch Starships data
 function sws_fetch_starships(){
@@ -26,28 +26,28 @@ function sws_fetch_starships(){
     $starships = json_decode($api_data, true)['results'];
     return $starships;
 }
+
+// Display Fetched data
 function sws_display_starships(){
     $starships = sws_fetch_starships();
     if (!$starships) {
         return 'Failed to fetch starships data.';
     }
-    $data = '<table>';
-    $data.= '<tr>
-    <th>Name</th>
-    <th>Class</th>
-    <th>Crew</th>
-    <th>Cost</th>
-    </tr>';
 
+    $data = '<table class="table table-bordered table-hover">';
+    $data .= '<thead class="thead-light"><tr><th>Name</th><th>Class</th><th>Crew</th><th>Cost in Credits</th></tr></thead>';
+    $data .= '<tbody>';
 
-    foreach ($starships as $starship) {
-        $data.='<tr>';
-        $data.='<td>'.$starship['name'].'</td>';
-        $data.='<td>'.$starship['starship_class'].'</td>';
-        $data.='<td>'.$starship['crew'].'</td>';
-        $data.='<td>'.$starship['cost_in_credits'].'</td>';
-        $data.='</tr>';
+    foreach ($starships as $ship) {
+        $data .= '<tr>';
+        $data .= '<td>' . esc_html($ship['name']) . '</td>';
+        $data .= '<td>' . esc_html($ship['starship_class']) . '</td>';
+        $data .= '<td>' . esc_html($ship['crew']) . '</td>';
+        $data .= '<td>' . esc_html($ship['cost_in_credits']) . '</td>';
+        $data .= '</tr>';
     }
+
+    $data .= '</tbody></table>';
     return $data;
 }
 add_shortcode('star_wars_starships', 'sws_display_starships');
