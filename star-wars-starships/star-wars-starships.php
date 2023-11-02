12fd07895221d4db0c12dcd5ae5605496fa28404
omerelias <omerelias123456@gmail.com>
2023-11-02 18:08:43 +0200

    Final plugin

--- star-wars-starships/star-wars-starships.php
+++ star-wars-starships/star-wars-starships.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Star Wars Starships
-Description: A plugin to display Star Wars starships from the SWAPI.
+Description: A plugin to display Star Wars starships from the SWAPI. Use the shortcode [star_wars_starships] to display the starships table on any page or post.
 Version: 1.0
 Author: Omer Elias
 */
@@ -26,14 +26,11 @@ function sws_fetch_starships(){
     $starships = json_decode($api_data, true)['results'];
     return $starships;
 }
-add_action('init','sws_fetch_starships'); 
-
 function sws_display_starships(){
-    $starships = fetch_starships();
+    $starships = sws_fetch_starships();
     if (!$starships) {
         return 'Failed to fetch starships data.';
     }
-
     $data = '<table>';
     $data.= '<tr>
     <th>Name</th>
@@ -42,11 +39,16 @@ function sws_display_starships(){
     <th>Cost</th>
     </tr>';
 
+
     foreach ($starships as $starship) {
         $data.='<tr>';
-        $data.='<td></td>';
+        $data.='<td>'.$starship['name'].'</td>';
+        $data.='<td>'.$starship['starship_class'].'</td>';
+        $data.='<td>'.$starship['crew'].'</td>';
+        $data.='<td>'.$starship['cost_in_credits'].'</td>';
         $data.='</tr>';
     }
+    return $data;
 }
 add_shortcode('star_wars_starships', 'sws_display_starships');
 
